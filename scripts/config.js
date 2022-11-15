a586b107ef3070d4d8b930cfbc62f8506b2009d3
Lennard K <104419069+LennardKu@users.noreply.github.com>
2022-11-15 14:45:30 +0100

    Update config.js
    Disable cash

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,8 @@
 (function(){
-  let discussionInit = document.createElement( "script" );
-  discussionInit.src = `https://cdn.kuenenwebsites.com/scripts/js/discussion.js`;
+  var versionUpdate = (new Date()).getTime(),
+  discussionInit = document.createElement( "script" );
+  discussionInit.src = `https://cdn.enervisie.com/scripts/js/discussion.js?v=${versionUpdate}`;
   discussionInit.setAttribute('defer',true);
-  discussionInit.setAttribute('siteKey','userpublicSiteKey');
+  discussionInit.setAttribute('siteKey','siteKey');
   document.getElementsByTagName( "head" )[0].appendChild( discussionInit );
 })();
