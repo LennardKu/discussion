39f38e28ae691193593687f6480e902aa061ad14
Lennard K <104419069+LennardKu@users.noreply.github.com>
2022-11-15 14:35:35 +0100

    Update discussion.php
    Setup for wordpress plugin

--- plugins/wordpress/discussion/discussion.php
+++ plugins/wordpress/discussion/discussion.php
@@ -1,1 +1,9 @@
-
+<?php
+/*
+*  Plugin Name: Discussion
+*  Version: 1.0.0
+*  Plugin Uri: https://github.com/LennardKu/plugins/wordpress/discussion/
+*  Description: Discussion 
+*  Author: Lennard Kuenen
+*  Author Uri:  https://discussion.kuenenwebsites.com
+*/
