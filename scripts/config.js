fdd8efc6a4183fd7c8a976cc9123bbe075b4b0e8
Lennard K <104419069+LennardKu@users.noreply.github.com>
2022-11-15 14:46:16 +0100

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 (function(){
   var versionUpdate = (new Date()).getTime(),
   discussionInit = document.createElement( "script" );
-  discussionInit.src = `https://cdn.enervisie.com/scripts/js/discussion.js?v=${versionUpdate}`;
+  discussionInit.src = `https://cdn.kuenenwebsites.com/scripts/js/discussion.js?v=${versionUpdate}`;
   discussionInit.setAttribute('defer',true);
   discussionInit.setAttribute('siteKey','siteKey');
   document.getElementsByTagName( "head" )[0].appendChild( discussionInit );
