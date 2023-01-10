5f3e23358e257208baf1e177dbf6a47f79db9843
Lennard K <104419069+LennardKu@users.noreply.github.com>
2023-01-10 10:43:29 +0100

    New update script
    without siteKey and diffrenct domain

--- scripts/config.js
+++ scripts/config.js
@@ -1,8 +1,7 @@
 (function(){
   var versionUpdate = (new Date()).getTime(),
-  discussionInit = document.createElement( "script" );
-  discussionInit.src = `https://cdn.kuenenwebsites.com/scripts/js/discussion.js?v=${versionUpdate}`;
-  discussionInit.setAttribute('defer',true);
-  discussionInit.setAttribute('siteKey','siteKey');
-  document.getElementsByTagName( "head" )[0].appendChild( discussionInit );
+  discussion = document.createElement( "script" );
+  discussion.src = `https://cdn.lennardkuenen.dev/scripts/js/discussion.js?v=${versionUpdate}`;
+  discussion.setAttribute('defer',true);
+  document.getElementsByTagName( "head" )[0].appendChild( discussion);
 })();
